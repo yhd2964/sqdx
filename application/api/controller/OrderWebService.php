cda65da70b86ac16da1d2cb3789a36dfdf4912f1
yanghuandi <you@example.com18829342342@163.com>
2020-04-28 18:58:37 +0800

    订单

--- application/api/controller/OrderWebService.php
+++ application/api/controller/OrderWebService.php
@@ -2,10 +2,27 @@
 namespace app\api\controller;
 
 use app\api\Model\Order;
+use think\console\command\make\Model;
 use think\Controller;
 use think\Request;
 
 class OrderWebService extends Controller{
+    //订单提交
+    public function submitOrderDetail(Request $request){
+        $param = $request->param();
+        $openId = $param['openId'];
+        $ShoppingCartList = $param['ShoppingCartList'];
+        $addressId = $param['addressId'];
+        $remark = $param['remark'];
+
+        $dataList = array();
+        //查询默认收货地址
+        $address = new Model('vshop_addresslibrary');
+        $address->find($addressId);
+
+
+    }
+
     //根据订单id取消订单    POST
     public function closeOrderByOrderId(Request $request){
         $orderId = $request->post('orderId');
