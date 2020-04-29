38bfad73c64bceb6629af988f29f32cb22af2ad7
yanghuandi <you@example.com18829342342@163.com>
2020-04-29 18:05:58 +0800

    列表

--- application/api/Model/PickUpAddress.php
+++ application/api/Model/PickUpAddress.php
@@ -4,4 +4,27 @@ use think\Model;
 class PickUpAddress extends Model {
     //用户自己选择的历史自提点列表
     protected $table = 'my_pick_up_address_list';
+    //根据用户openid获取用户历史自提点列表
+    public function getAddressListByOpenid($openid){
+        $model = new PickUpAddress;
+        $res = $model->where(array('openid'=>$openid))->select();
+
+        $array = array();
+        $i = 0;
+        foreach ($res as $key=>$value){
+            $array[$i]['id'] = $value->data['id'];
+            $array[$i]['self_pick_up_id'] = $value->data['self_pick_up_id'];
+            $array[$i]['self_pick_up_name'] = $this->getSelfPickUpNameById($value->data['self_pick_up_id']);
+            $i++;
+        }
+
+        return $array;
+    }
+
+    protected function getSelfPickUpNameById($id){
+        $model = new PickUpList();
+        $res = $model->where(['id'=>$id])->find();
+        //var_dump($res->data);
+        return $res->data['name'];
+    }
 }
--- application/api/controller/Address.php
+++ application/api/controller/Address.php
@@ -13,18 +13,18 @@ class Address extends Controller{
             $data = ['status' => 'error', 'msg' => '缺少参数'];
             return json($data);
         }
-        //自提点id
-        $addressId = $param['addressId'];
+        //社区电商自提点id
+        $self_pick_up_id = $param['self_pick_up_id'];
         $openid = $param['openid'];
 
         $model = new PickUpAddress();
         $model->openid = $openid;
-        $model->self_pick_up_id = $addressId;
+        $model->self_pick_up_id = $self_pick_up_id;
         $model->is_default = 0;
         $model->create_time = date('Y-m-d H:i:s');
         $model->save();
 
-        $data = ['status' => 'success', 'msg' => '地址添加成功'];
+        $data = ['status' => 'success', 'msg' => '自提点地址添加成功'];
         return json($data);
     }
 
@@ -35,7 +35,7 @@ class Address extends Controller{
             $data = ['status' => 'error', 'msg' => '缺少参数addressId'];
             return json($data);
         }
-        //自提点id
+        //需要删除的自提点地址id
         $addressId = $param['addressId'];
 
         $res = PickUpAddress::destroy(['id'=>$addressId]);
@@ -48,9 +48,17 @@ class Address extends Controller{
         }
     }
 
-    //获取用户的历史自提点列表
+    //根据用户openid 获取用户的历史自提点列表
     public function getAddressListByOpenid(Request $request){
         $param = $request->param();
+        if (empty($param['openid'])){
+            return json(['status' => 'error', 'msg' => '缺少参数openid']);
+        }
 
+        $openid = $param['openid'];
+        $model = new PickUpAddress();
+        $res = $model->getAddressListByOpenid($openid);
+
+        return json(['status'=>'success','data'=>$res]);
     }
 }
